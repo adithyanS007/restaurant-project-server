158ce0467936c67e96cd2e0075d9645d45f67fd5
adithyanS007 <adhiadithyan21@gmail.com>
2025-01-28 00:04:33 +0530

    url added to cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const brunchRoutes = require('./routes/brunchRoutes');
 const app = express();
 // CORS options to allow multiple origins (http://localhost:5173 and http://localhost:5174)
 const corsOptions = {
-  origin: ['https://restaurant-project-client-kappa.vercel.app'], // Allow both frontend ports
+  origin: ['https://restaurant-project-client-kappa.vercel.app', 'http://localhost:5173'], // Allow both frontend ports
   methods: ['GET', 'POST'],
   credentials: true
 };
